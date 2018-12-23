bf84c42c45d46856a59f64ddd6a4f8977cca17eb
Mariana Mărășoiu <mariana.marasoiu@gmail.com>
2018-12-23 21:53:27 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name="release-testing",
+    version="0.0.1",
+    url="https://github.com/marianamarasoiu/release-testing",
+    packages=["release_testing"]
+)
